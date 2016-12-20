25473620ad6b74225a426c27b868845924271a6c
Street Money <mutuadavid93@gmail.com>
2016-12-20 16:27:44 +0300

    Fnished Feedback form

--- app/public/js/feedback.js
+++ app/public/js/feedback.js
@@ -2,17 +2,44 @@ $(function () {
     //get data from the api route
    $.getJSON('api', updateFeedback);
    
+   //POST verb
+   //write to file.  
+   $('.feedback-form').submit(function (e) {
+       e.preventDefault();
+       
+       $.post('api', {
+           name: $('#feedback-form-name').val(),
+           title: $('#feedback-form-title').val(),
+           message: $('#feedback-form-message').val()
+       }, updateFeedback);//instant update page.
+   });
    
+   //Delete a message
+   $('.feedback-messages').on('click', function (evt) {
+       if(evt.target.className === 'glyphicon glyphicon-remove') {
+           $.ajax({
+               url: 'api/' + evt.target.id,
+               type: 'DELETE',
+               success: updateFeedback
+           });//ajax
+       }//if
+   });
    
+   //retrieve data
     function updateFeedback(data) {
         var output = '';
         $.each(data, function (key, item) {
             
             output += '    <div class="feedback-item item-list media-list">';
             output += '        <div class="feedback-item media">';
+            
+            //delete btn
+            output += '        <div class="media-left"><button class="feedback-delete btn btn-xs btn-danger">\n\
+ <span id='+key+' class="glyphicon glyphicon-remove"></span></button></div>';
+            
             output += '          <div class="feedback-info media-body">';
             output += '            <div class="feedback-head">';
-            output += '              <div class="feedback-title">'+item.title+'<small class="feedback-name label label-info"></small></div>';
+            output += '              <div class="feedback-title">'+item.title+' &nbsp;<small class="feedback-name label label-info">'+item.name+'</small></div>';
             output += '            </div>';                                                      
             output += '            <div class="feedback-message">'+item.message+'</div>';
             output += '          </div>';
--- app/routes/api.js
+++ app/routes/api.js
@@ -2,13 +2,50 @@
 //Create the router instance
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
-var feedbackData = require('../../data/feedback.json');
+//A middleware to handle post requests
+var bodyParser = require('body-parser');
+
+var feedbackData = require('../data/feedback.json');
 
 router.get('/api', function (req, res) {
     //spit the data inform of json
      res.json(feedbackData);
 });
+ 
+//use the middleware
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended:false }));//a simple form
+
+//the actual post method on a route
+router.post('/api', function (req, res) {
+    feedbackData.unshift(req.body);
+    
+    //persist data even on server restart
+    fs.writeFile('app/data/feedback.json', JSON.stringify(feedbackData), 'utf8', function(err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+    
+    res.json(feedbackData);
+});
+
+//Delete a message
+router.delete('/api/:id', function(req, res) {
+    feedbackData.splice(req.params.id, 1);    
+    //persist data even on server restart
+    fs.writeFile('app/data/feedback.json', JSON.stringify(feedbackData), 'utf8', function(err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+    
+    res.json(feedbackData);
+});
+
+
 
 //Expose this route to app.js
 module.exports = router;
